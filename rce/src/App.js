b7c244fa929b2bfe197f0fad02aef34662be39a9
Evan Yang <evanjyang4@gmail.com>
2018-07-11 15:52:12 -0700

    Some React Components for the Homepage

--- rce/src/App.js
+++ rce/src/App.js
@@ -1,1 +1,29 @@
-alert('It\'s working');
+class Homepage extends React.Component {
+  render(){
+    return (
+      <div>
+        <Logo />
+        <Login />
+      </div>
+    );
+  }
+
+}
+
+class Logo extends React.Component {
+  render() {
+    return (
+      <h1> RCE </h1>
+    );
+  }
+}
+
+class Login extends React.Component {
+  render(){
+    return (
+      <button> Log In </button>
+    );
+  }
+}
+
+ReactDOM.render(<Homepage />, document.getElementById('app'));
