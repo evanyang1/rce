0e0f5f47533c1ecff049c9b7f3e4474abf9f6186
Evan Yang <evanjyang4@gmail.com>
2018-07-18 14:27:06 -0700

    set up project for SCSS styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Homepage from './components/Homepage';
-
+import './styles/styles.css';
 ReactDOM.render(<Homepage />, document.getElementById('app'));
