8e67500ab4401eb924a01a63ca7fd17b48f8cd4e
Evan Yang <evanjyang4@gmail.com>
2018-07-23 13:54:47 -0700

    developed Login page further: formatting, passwords, naming

--- src/Components/Pages/LoginPage.js
+++ src/Components/Pages/LoginPage.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { BrowserRouter, Route, Link, withRouter } from "react-router-dom";
 
 const LoginPage = () => (
-  <div>
-    <h2> Log In </h2>
-      <form>
-        First name:<br />
-        <input type="text" name="firstname" />
-        <br />
-        Last name:<br />
-        <input type="text" name="lastname" />
-        <br /><br />
-        <input type="submit" value="Submit" />
-      </form>
+  <div className="container">
+    <div className="jumbotron">
+        <h2> Log In </h2>
+          <form>
+            Email:<br />
+            <input type="text" name="Email" />
+            <br />
+            Password:<br />
+            <input type="password" name="Password" />
+            <br /><br />
+            <input type="submit" value="Submit" />
+          </form>
+
+    </div>
   </div>
 );
 export default withRouter(LoginPage);
